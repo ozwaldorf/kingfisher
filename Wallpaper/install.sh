0a9d38def586d6b441f0b65aa1ab63885651ad09
Wolfgange3311999 <matthew3311999@gmail.com>
2016-04-27 10:39:45 -0500

    Various changes
    - Script can now be installed from any location (as a result, the
    desktop file won't work until install.sh is run)
    - The un-blurred image is kept in case the user wants to reveal what is
    behind the blur.
    - The text in both overlays now has a shadow.
    - The desktop file now uses a budgie logo

--- Wallpaper/install.sh
+++ Wallpaper/install.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
-cp Wallpaper.desktop ~/.config/autostart/Wallpaper.desktop
+
+called="$BASH_SOURCE"
+if [ "$called" = "" ]; then
+    called="$0"
+fi
+folder=$(dirname -- $(readlink -fn -- "$called"))
+
+while true; do
+    read -p "Are you in the location you would like to keep the script? (y/N)" yn
+
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Please navigate there and restart the script"; exit;;
+        * ) echo "Please navigate there and restart the script"; exit;;
+    esac
+done
+
+#Hardcode the folder into the desktop file
+processed_folder=$(echo $folder | sed -e 's/[\/&]/\\&/g')
+sed -i 's/!!LOCATION!!/'$processed_folder'/' "$folder/Wallpaper.desktop"
+
+cp "$folder/Wallpaper.desktop" ~/.config/autostart/Wallpaper.desktop
+echo "Installation complete!"
 rm install.sh
