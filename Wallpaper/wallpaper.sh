82a893991014eb1262369a54e87cc0201e0410b3
Ossian Mapes <the5heepdev@gmail.com>
2016-04-27 12:14:17 -0400

    Fixes for @Wolfgange3311999 version

--- Wallpaper/wallpaper.sh
+++ Wallpaper/wallpaper.sh
@@ -10,7 +10,7 @@ picOpts="zoom"
 raw_image_name="source.jpg"
 
 #Check and Remove Old Images
-old_file=$(find $folder -maxdepth 1 -regex ".*/wallpaper-[0-9]+.jpg" | head -n 1)
+old_file=$(find $folder -maxdepth 1 -regex ".*/wallpaper.jpg" | head -n 1)
 if [ -e "$old_file" ]; then
   rm "$old_file"
 fi
@@ -23,14 +23,14 @@ curl -s -o "$folder/$raw_image_name" -L https://unsplash.it/1920/1200/\?random
 
 #Generate File Name
 rand_num=$(od -An -N2 -i /dev/random | tr -d ' ')
-file_name="wallpaper-$rand_num.jpg"
+file_name="wallpaper.jpg"
 
 #Blur Image
 convert "$folder/$raw_image_name" -blur 160x80 "$folder/$file_name"
 
 #Add overlay
-composite -gravity Center "$folder/overlay.png" "$folder/$file_name" "$folder/$file_name"
+composite -gravity Center "$folder/assets/overlay.png" "$folder/$file_name" "$folder/$file_name"
 
 #Set Wallpaper
-gsettings set org.gnome.desktop.background picture-uri '"'$folder'/'$file_name'"'
-gsettings set org.gnome.desktop.screensaver picture-uri '"'$folder'/'$file_name'"'
+gsettings set org.gnome.desktop.background picture-uri "'$folder'/wallpaper.jpg"
+gsettings set org.gnome.desktop.screensaver picture-uri "'$folder'/wallpaper.jpg"
